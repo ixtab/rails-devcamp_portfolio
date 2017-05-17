7e354c4288373daabc81f93856140015f4417172
Jose Carlos Calzada <josecarlosplatos@gmail.com>
2017-05-17 18:41:27 +0200

    Implementada la opción Show para el Portfolio

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -26,7 +26,7 @@ class PortfoliosController < ApplicationController
 
   def update
     @portfolio_item = Portfolio.find(params[:id])
-    
+
     respond_to do |format|
       if @portfolio_item.update(params.require(:portfolio).permit(:title, :subtitle, :body))
         format.html { redirect_to portfolios_path, notice: 'Se ha modificado el artículo del Portfolio.' }
@@ -36,6 +36,8 @@ class PortfoliosController < ApplicationController
     end
   end
 
-
+  def show
+    @portfolio_item = Portfolio.find(params[:id])
+  end
 
 end
