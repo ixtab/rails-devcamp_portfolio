9f722c6497d1a3781dc52c1139355773eaa5e3cd
Jose Carlos Calzada <josecarlosplatos@gmail.com>
2017-05-28 19:39:52 +0200

    Implemented collection based partials

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,27 +1,27 @@
 class PortfoliosController < ApplicationController
   layout 'portfolio'
   def index
-    @portfolio_item = Portfolio.all
+    @portfolio_items = Portfolio.all
   end
 
   def angular
-    @angular_portfolio_item = Portfolio.angular
+    @angular_portfolio_items = Portfolio.angular
   end
 
   def ruby
-    @rails_portfolio_item = Portfolio.ruby
+    @rails_portfolio_items = Portfolio.ruby
   end
 
   def new
-    @portfolio_item = Portfolio.new
-    3.times { @portfolio_item.technologies.build }
+    @portfolio_items = Portfolio.new
+    3.times { @portfolio_items.technologies.build }
   end
 
   def create
-    @portfolio_item = Portfolio.new(portfolio_params)
+    @portfolio_items = Portfolio.new(portfolio_params)
 
     respond_to do |format|
-      if @portfolio_item.save
+      if @portfolio_items.save
         format.html { redirect_to portfolios_path, notice: 'Se ha creado el objeto del Portfolio.' }
       else
         format.html { render :new }
@@ -30,14 +30,14 @@ class PortfoliosController < ApplicationController
   end  
 
   def edit
-    @portfolio_item = Portfolio.find(params[:id])
+    @portfolio_items = Portfolio.find(params[:id])
   end
 
   def update
-    @portfolio_item = Portfolio.find(params[:id])
+    @portfolio_items = Portfolio.find(params[:id])
 
     respond_to do |format|
-      if @portfolio_item.update(portfolio_params)
+      if @portfolio_items.update(portfolio_params)
         format.html { redirect_to portfolios_path, notice: 'Se ha modificado el artículo del Portfolio.' }
       else
         format.html { render :edit }
@@ -46,15 +46,15 @@ class PortfoliosController < ApplicationController
   end
 
   def show
-    @portfolio_item = Portfolio.find(params[:id])
+    @portfolio_items = Portfolio.find(params[:id])
   end
 
   def destroy
     # Perform the lookout
-    @portfolio_item = Portfolio.find(params[:id])
+    @portfolio_items = Portfolio.find(params[:id])
 
     # Destroy(delete) the record
-    @portfolio_item.destroy
+    @portfolio_items.destroy
 
     # Redireccionar a la página
     respond_to do |format|
